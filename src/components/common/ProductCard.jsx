9e13115e51d1ee9fe276a3b68acdcd63539a0bc0
naatmayman <nhat.pham.tda@gmail.com>
2024-09-10 13:29:35 +0700

    Initial commit

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -105,9 +105,9 @@ const ProductCard = ({
           {className === "List" ? (
             <div className={styles["product-name"]}>{name}</div>
           ) : (
-            <div className={styles["product-name"]}>
+            <Link to={`/shop/${id}`} className={styles["product-name"]}>
               <LinesEllipsis text={name} maxLine="2" ellipsis="..." trimRight basedOn="letters" />
-            </div>
+            </Link>
           )}
           <div className={styles["product-stars"]}>
             <StarRatings rating={starrate} starRatedColor="#ffd700" numberOfStars={5} />
